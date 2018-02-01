d3fbc10ad407f1f7abb66bea017fa79cda491d1b
Sofyan Sitorus <sofyansitorus@gmail.com>
2018-02-01 09:55:38 +0700

    Remove Maps Place Picker

--- includes/class-woogosend.php
+++ includes/class-woogosend.php
@@ -127,14 +127,22 @@ class WooGoSend extends WC_Shipping_Method {
 				'desc_tip'    => true,
 			),
 			'gmaps_api_key'             => array(
-				'title'       => __( 'API Key', 'woogosend' ),
+				'title'       => __( 'Google Maps Distance Matrix API', 'woogosend' ),
 				'type'        => 'text',
 				'description' => __( 'This plugin require Google Maps Distance Matrix API Services enabled in your Google Console. <a href="https://developers.google.com/maps/documentation/distance-matrix/get-api-key" target="_blank">Click here</a> to get API Key and to enable the services.', 'woogosend' ),
 				'default'     => '',
 			),
-			'gmaps_address_picker'      => array(
-				'title' => __( 'Store Location', 'woogosend' ),
-				'type'  => 'address_picker',
+			'origin_lat'                => array(
+				'title'       => __( 'Store Location Latitude', 'woogosend' ),
+				'type'        => 'decimal',
+				'description' => __( '<a href="http://www.latlong.net/" target="_blank">Click here</a> to get your store location coordinates info.', 'woogosend' ),
+				'default'     => '',
+			),
+			'origin_lng'                => array(
+				'title'       => __( 'Store Location Longitude', 'woogosend' ),
+				'type'        => 'decimal',
+				'description' => __( '<a href="http://www.latlong.net/" target="_blank">Click here</a> to get your store location coordinates info.', 'woogosend' ),
+				'default'     => '',
 			),
 			'gmaps_api_mode'            => array(
 				'title'       => __( 'Travel Mode', 'woogosend' ),
@@ -160,14 +168,6 @@ class WooGoSend extends WC_Shipping_Method {
 					'ferries'  => __( 'Avoid Ferries', 'woogosend' ),
 					'indoor'   => __( 'Avoid Indoor', 'woogosend' ),
 				),
-			),
-			'origin_lat'                => array(
-				'type'    => 'hidden',
-				'default' => '',
-			),
-			'origin_lng'                => array(
-				'type'    => 'hidden',
-				'default' => '',
 			),
 			'tax_status'                => array(
 				'title'   => __( 'Tax status', 'woogosend' ),
@@ -357,68 +357,6 @@ class WooGoSend extends WC_Shipping_Method {
 	}
 
 	/**
-	 * Generate origin settings field.
-	 *
-	 * @since 1.0.0
-	 * @param string $key Settings field key.
-	 * @param array  $data Settings field data.
-	 */
-	public function generate_address_picker_html( $key, $data ) {
-		$field_key = $this->get_field_key( $key );
-
-		$defaults = array(
-			'title'             => '',
-			'disabled'          => false,
-			'class'             => '',
-			'css'               => '',
-			'placeholder'       => '',
-			'type'              => 'text',
-			'desc_tip'          => false,
-			'description'       => '',
-			'custom_attributes' => array(),
-			'options'           => array(),
-		);
-
-		$data = wp_parse_args( $data, $defaults );
-
-		ob_start(); ?>
-		<tr valign="top">
-			<th scope="row" class="titledesc">
-				<?php echo esc_html( $this->get_tooltip_html( $data ) ); ?>
-				<label for="<?php echo esc_attr( $field_key ); ?>"><?php echo wp_kses_post( $data['title'] ); ?></label>
-			</th>
-			<td class="forminp">
-				<div id="woogosend-map-wrapper" class="woogosend-map-wrapper"></div>
-				<script type="text/html" id="tmpl-woogosend-map-search">
-					<input id="{{data.map_search_id}}" class="woogosend-map-search controls" type="text" placeholder="<?php echo esc_attr( __( 'Search your store location', 'woogosend' ) ); ?>" autocomplete="off" />
-				</script>
-				<script type="text/html" id="tmpl-woogosend-map-canvas">
-					<div id="{{data.map_canvas_id}}" class="woogosend-map-canvas"></div>
-				</script>
-			</td>
-		</tr>
-		<?php
-		return ob_get_clean();
-	}
-
-	/**
-	 * Generate hidden settings field.
-	 *
-	 * @since 1.0.0
-	 * @param string $key Settings field key.
-	 * @param array  $data Settings field data.
-	 */
-	public function generate_hidden_html( $key, $data ) {
-		$field_key = $this->get_field_key( $key );
-
-		ob_start();
-		?>
-		<input type="hidden" name="<?php echo esc_attr( $field_key ); ?>" id="<?php echo esc_attr( $field_key ); ?>" value="<?php echo esc_attr( $this->get_option( $key ) ); ?>">
-		<?php
-		return ob_get_clean();
-	}
-
-	/**
 	 * Validate gmaps_api_key settings field.
 	 *
 	 * @since    1.0.0
@@ -544,7 +482,7 @@ class WooGoSend extends WC_Shipping_Method {
 			return;
 		}
 
-		// Trasnlators: Number of drivers needed.
+		// translators: Number of drivers needed.
 		$drivers_count_text = sprintf( _n( '%s driver', '%s drivers', $drivers_count, 'woogosend' ), $drivers_count );
 
 		$shipping_cost_total = $this->per_km_cost_instant * $api_request['distance'];
@@ -621,7 +559,7 @@ class WooGoSend extends WC_Shipping_Method {
 			return;
 		}
 
-		// Trasnlators: Number of drivers needed.
+		// translators: Number of drivers needed.
 		$drivers_count_text = sprintf( _n( '%s driver', '%s drivers', $drivers_count, 'woogosend' ), $drivers_count );
 
 		$shipping_cost_total = $this->min_cost_same_day;
